78bd926b176afa36100e43efc10e5ff007fa1f1a
lucasbaptist <lucasbaptista@me.com>
2023-10-23 13:56:58 -0300

    initial commit

---
+++ GymApp Watch App/GymAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymAppApp.swift
+//  GymApp Watch App
+//
+//  Created by Lucas Baptista on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GymApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
