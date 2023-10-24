130e15d7538589e14df307fc6b1b71d6ab406787
lucasbaptist <lucasbaptista@me.com>
2023-10-24 15:00:03 -0300

    - UI Tela Principal - Desenho do Cronômetro

--- GymApp Watch App/ContentView.swift
+++ GymApp Watch App/ContentView.swift
@@ -8,17 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var stopWatchManager = StopWatchManager()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            
+            
+            Circle()
+                .stroke()
+                .frame(width: 185, height: 185)
+                .offset(y: -25)
+            
+            VStack{
+                
+                Text("Exercício 1")
+                    .font(.subheadline)
+                    .padding(.bottom)
+                Text("Série 1")
+                    .font(.title)
+                Text(String(format: "%1.1f", stopWatchManager.secondsElapsed))
+                    .padding(.top)
+                    .monospaced()
+    
+            }
+            .padding(.bottom, 45.0)
+            
+            VStack {
+                
+                Spacer()
+                
+                HStack{
+                    
+                    Button {
+                        stopWatchManager.start()
+                    } label: {
+                        Image(systemName: "arrow.clockwise")
+                            .frame(width: 43, height: 43)
+                            .foregroundColor(Color.white)
+                            .background(Color.gray)
+                            .clipShape(Circle())
+                    }
+                   
+                    .frame(width: 43, height: 35)
+                    .buttonStyle(.borderless)
+                    
+                    Spacer()
+                    
+                    Button {
+                        print("next page")
+                    } label: {
+                        Image(systemName: "chevron.forward.2")
+                            .frame(width: 43, height: 43)
+                            .foregroundColor(Color.black)
+                            .background(Color.yellow)
+                            .clipShape(Circle())
+                    }
+                    .frame(width: 43, height: 35)
+                    .buttonStyle(.borderless)
+                    
+                }
+            }
+            .padding(.horizontal)
+            .padding(.bottom, 16)
+            .ignoresSafeArea(edges: .bottom)
+                  
+            
         }
-        .padding()
+        
+        .background(
+            LinearGradient(gradient: Gradient(colors: [Color("GreyBG"), Color.black]), startPoint: .init(x: 0.5, y: -0.4), endPoint: .bottom)
+        )
     }
+    
 }
+
+
+
 
 #Preview {
     ContentView()
 }
+
